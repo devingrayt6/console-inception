9e3838a5c7ba1e0ddb406211e7465a5096d00a12
devingrayt6 <devingrayt6@gmail.com>
2020-03-30 09:50:53 -0600

    touchup

--- app/Models/Game.cs
+++ app/Models/Game.cs
@@ -2,64 +2,73 @@ using inception.Interfaces;
 
 namespace inception.Models
 {
-    class Game : IGame
+  class Game : IGame
+  {
+    public IPlayer CurrentPlayer { get; set; }
+    public IRoom CurrentRoom { get; set; }
+
+    ///<summary>Initalizes data and establishes relationships</summary>
+    public Game()
     {
-        public IPlayer CurrentPlayer { get; set; }
-        public IRoom CurrentRoom { get; set; }
-
-        ///<summary>Initalizes data and establishes relationships</summary>
-        public Game()
-        {
-            // NOTE ALL THESE VARIABLES ARE REMOVED AT THE END OF THIS METHOD
-            // We retain access to the objects due to the linked list
+      // NOTE ALL THESE VARIABLES ARE REMOVED AT THE END OF THIS METHOD
+      // We retain access to the objects due to the linked list
 
 
-            // NOTE Create all rooms
+      // NOTE Create all rooms
 
-            // First level rooms
-            Room middlePlane = new Room("middle of plane", "You are are in the middle row of a medium sized airplane. You know your target is sitting somewhere on the plane. Use 'forward' and 'backward' to find your target. Use 'incept' to dive into his/her dreams when you do.");
-            Room backPlane = new Room("back of plane", "You are at the back of the plane. You do not see your target, but you do see a rodent. Not a very clean plane...");
-            Room frontPlane = new Room("front of plane", "You are at the front of the plane. You do not see your target. You do however notice that there are no airbags and only one parachute in case of an emergency...");
-            Room cockPit = new Room("cockpit", "You are in the cockpit with the pilot and co pilot. You realize the pilot is your target and that the co pilot is currently asleep... incepting the pilot will knock him out...");
-           
-            // Second level Rooms
-            Room darkRoom = new Room("Dark Room", "You are in a dark room and can't see a thing. You do however feel something dangling at about eye level. Feels like a small chain of some type.");
-            Room classRoom = new Room("Class Room", "You seem to be in mr. Yangs old highschool math class. It also seems...you're not wearing pants! How embarrassing! Everyone is laughing at you!! Looks like there's a door to the north...");
-            Room hallWay = new Room("Hall Way", "You are in the Hall Way...There is a door at the end of the hall (east).");
-            Room tvRoom= new Room("tvRoom", "There is a couch in front of you with a man who you recognize as mr. Yang watching television. You look at the TV and realize the movie is about you! You seem to be starring in The Breakfast Club... as the teacher...");
+      // First level rooms
+      Room middlePlane = new Room("middle of plane", "You are are in the middle row of a medium sized airplane. You know your target is sitting somewhere on the plane. Use 'forward' and 'backward' to find your target. Use 'incept' to dive into his/her dreams when you do.");
+      Room backPlane = new Room("back of plane", "You are at the back of the plane. You do not see your target, but you do see a rodent. Not a very clean plane...");
+      Room frontPlane = new Room("front of plane", "You are at the front of the plane. You do not see your target. You do however notice that there are no airbags and only one parachute in case of an emergency...");
+      Room cockPit = new Room("cockpit", "You are in the cockpit with the pilot and co pilot. You realize the pilot is your target and that the co pilot is currently asleep... incepting the pilot will knock him out...");
 
-            // Third level Rooms
-            Room safeRoom = new Room("Safe Room", "You are in an empty room that is completely white. Floor, ceiling, walls...All white except for a black safe in the middle. The safe has a keypad with [1][2][3][4]. Type [passcode: <thepasscode>] to crack open the safe!");
-            
-            // NOTE Create all Items
-            Item chain = new Item("chain", "A small chain that is hanging above your head...");
+      // Second level Rooms
+      Room darkRoom = new Room("Dark Room", "You are in a dark room and can't see a thing. You do however feel something dangling at about eye level. Feels like a small chain of some type.");
+      Room classRoom = new Room("Class Room", "You seem to be in mr. Yangs old highschool math class. It also seems...you're not wearing pants! How embarrassing! Everyone is laughing at you!! Looks like there's a door to the north...");
+      Room hallWay = new Room("Hall Way", "You are in the Hall Way...There is a door at the end of the hall (east) that appears to be locked.");
+      Room tvRoom = new Room("tvRoom", "There is a couch in front of you with a man who you recognize as mr. Yang watching television. You look at the TV and realize the movie is about you! You seem to be starring in The Breakfast Club... as the teacher...");
 
-            // NOTE Make Room Relationships
+      // Third level Rooms
+      Room safeRoom = new Room("Safe Room", "You are in an empty room that is completely white. Floor, ceiling, walls...All white except for a black safe in the middle. The safe has a keypad with [1][2][3][4]. Type [passcode: <thepasscode>] to crack open the safe!");
 
-            //First Dream Level 
-            backPlane.Exits.Add("forward", middlePlane);
-            middlePlane.Exits.Add("backward", backPlane);
-            middlePlane.Exits.Add("forward", frontPlane);
-            frontPlane.Exits.Add("backward", middlePlane);
-            frontPlane.Exits.Add("forward", cockPit);
-            cockPit.Exits.Add("backward", frontPlane);
-
-            // Second Dream Level
-            darkRoom.Exits.Add("switch", classRoom);
-            classRoom.Exits.Add("north", hallWay);
-            hallWay.Exits.Add("east", tvRoom);
-            hallWay.Exits.Add("south", classRoom);
-            tvRoom.Exits.Add("west", hallWay);
-
-            // Set inception relationships
-            cockPit.InceptTo = darkRoom;
-            tvRoom.InceptTo = safeRoom;
+      // NOTE Create all Items
+      Item chain = new Item("chain", "A small chain that is hanging above your head...");
+      Item note1 = new Item("note", "A crumpled paper. A note that says pass: 11...");
+      Item note2 = new Item("note", "A crumpled paper. A note that says pass: 12...");
+      Item key = new Item("key", "A gold key...[use key] to use this item.");
 
 
-            // NOTE put Items in Rooms
-            darkRoom.Items.Add(chain);
 
-            CurrentRoom = middlePlane;
-        }
+      // NOTE Make Room Relationships
+
+      //First Dream Level 
+      backPlane.Exits.Add("forward", middlePlane);
+      middlePlane.Exits.Add("backward", backPlane);
+      middlePlane.Exits.Add("forward", frontPlane);
+      frontPlane.Exits.Add("backward", middlePlane);
+      frontPlane.Exits.Add("forward", cockPit);
+      cockPit.Exits.Add("backward", frontPlane);
+
+      // Second Dream Level
+      darkRoom.Exits.Add("switch", classRoom);
+      classRoom.Exits.Add("north", hallWay);
+      hallWay.AddLockedRoom(key, "east", tvRoom);
+      hallWay.Exits.Add("south", classRoom);
+      tvRoom.Exits.Add("west", hallWay);
+
+      // Set inception relationships
+      cockPit.InceptTo = darkRoom;
+      tvRoom.InceptTo = safeRoom;
+
+
+      // NOTE put Items in Rooms
+      darkRoom.Items.Add(chain);
+      backPlane.Items.Add(note1);
+      classRoom.Items.Add(note2);
+      frontPlane.Items.Add(key);
+
+
+      CurrentRoom = middlePlane;
     }
+  }
 }
--- app/Services/GameService.cs
+++ app/Services/GameService.cs
@@ -5,150 +5,162 @@ using inception.Models;
 
 namespace inception.Services
 {
-    class GameService : IGameService
+  class GameService : IGameService
+  {
+    public List<string> Messages { get; set; }
+    private IGame _game { get; set; }
+
+    public GameService(string playerName)
     {
-        public List<string> Messages { get; set; }
-        private IGame _game { get; set; }
+      Messages = new List<string>();
+      _game = new Game();
+      _game.CurrentPlayer = new Player(playerName);
+      Look();
+    }
 
-        public GameService(string playerName)
+    public bool Go(string direction)
+    {
+      //if the current room has that direction on the exits dictionary
+      if (_game.CurrentRoom.Exits.ContainsKey(direction))
+      {
+        // set current room to the exit room
+        _game.CurrentRoom = _game.CurrentRoom.Exits[direction];
+        // populate messages with room description
+        Messages.Add($"You Travel {direction}, and discover: ");
+        Look();
+        EndRoom end = _game.CurrentRoom as EndRoom;
+        if (end != null)
         {
-            Messages = new List<string>();
-            _game = new Game();
-            _game.CurrentPlayer = new Player(playerName);
-            Look();
+          Messages.Add(end.Narrative);
+          return false;
         }
+        return true;
+      }
+      //no exit in that direction
+      Messages.Add("No Room in that direction");
+      Look();
+      return true;
+    }
 
-        public bool Go(string direction)
-        {
-            //if the current room has that direction on the exits dictionary
-            if (_game.CurrentRoom.Exits.ContainsKey(direction))
-            {
-                // set current room to the exit room
-                _game.CurrentRoom = _game.CurrentRoom.Exits[direction];
-                // populate messages with room description
-                Messages.Add($"You Travel {direction}, and discover: ");
-                Look();
-                EndRoom end = _game.CurrentRoom as EndRoom;
-                if (end != null)
-                {
-                    Messages.Add(end.Narrative);
-                    return false;
-                }
-                return true;
-            }
-            //no exit in that direction
-            Messages.Add("No Room in that direction");
-            Look();
-            return true;
-        }
+    public bool Incept()
+    {
+      if (_game.CurrentRoom.InceptTo != null)
+      {
+        _game.CurrentRoom = _game.CurrentRoom.InceptTo;
+        Look();
+        return true;
+      }
+      else if (_game.CurrentRoom.Name == "Safe Room")
+      {
+        Console.WriteLine("You have gone one dream to deep...You lose your grip on reality and dream for an eternity until...YOU DIE!");
+        return false;
+      }
+      else
+      {
+        Console.WriteLine("You must be in the same room as your target to incept them!");
+        return true;
+      }
+    }
 
-        public bool Incept()
-        {
-            if(_game.CurrentRoom.InceptTo != null)
-            {
-            _game.CurrentRoom = _game.CurrentRoom.InceptTo;
-            Look();
-            return true;
-            }
-            else if(_game.CurrentRoom.Name == "Produce Section")
-            {
-                Console.WriteLine("You have gone one dream to deep...You lose your grip on reality and dream for an eternity until...YOU DIE!");
-                return false;
-            }
-            else{
-                Console.WriteLine("You must be in the same room as your target to incept them!");
-                return true;
-            }
-        }
-
-        public void Help()
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public void Inventory()
-        {
-            Messages.Add("Current Inventory: ");
-            foreach (var item in _game.CurrentPlayer.Inventory)
-            {
-                Messages.Add($"{item.Name} - {item.Description}");
-            }
-        }
-
-        public void Look()
-        {
-            Messages.Add(_game.CurrentRoom.Name);
-            Messages.Add(_game.CurrentRoom.Description);
-            if (_game.CurrentRoom.Items.Count > 0)
-            {
-                Messages.Add("There Are a few things in this room:");
-                foreach (var item in _game.CurrentRoom.Items)
-                {
-                    Messages.Add("     " + item.Name);
-                }
-            }
-            // string exits = string.Join(", ", _game.CurrentRoom.Exits.Keys);
-            // Messages.Add("There are exits to the " + exits);
-
-            // string lockedExits = "";
-            // foreach (var lockedRoom in _game.CurrentRoom.LockedExits.Values)
-            // {
-            //     lockedExits += lockedRoom.Key;
-            // }
-            // Messages.Add("There are locked exits to the " + lockedExits);
-
-        }
-
-        public void Reset()
-        {
-            string name = _game.CurrentPlayer.Name;
-            _game = new Game();
-            _game.CurrentPlayer = new Player(name);
-        }
-
-        public void Take(string itemName)
-        {
-            IItem found = _game.CurrentRoom.Items.Find(i => i.Name.ToLower() == itemName);
-            if (found != null)
-            {
-                _game.CurrentPlayer.Inventory.Add(found);
-                _game.CurrentRoom.Items.Remove(found);
-                Messages.Add($"You have taken the {itemName}");
-                return;
-            }
-            Messages.Add("Cannot find item by that name");
-        }
-
-        public void Use(string itemName)
-        {
-            var found = _game.CurrentPlayer.Inventory.Find(i => i.Name.ToLower() == itemName);
-            var foundInRoom = _game.CurrentRoom.Items.Find(i => i.Name.ToLower() == itemName);
-
-            if (found != null)
-            {
-                Messages.Add(_game.CurrentRoom.Use(found));
-            }
-            else if(itemName == "chain")
-            {
-                    _game.CurrentRoom = _game.CurrentRoom.Exits["switch"];
-                    Look();
-            }else{
-            Messages.Add("You don't have that Item");
-            }
-            // check if item is in Inventory
-            return;
-        }
-
-        public bool CrackSafe(string code)
-        {
-            if(code == "1112")
-            {
-                Messages.Add("CONGRATULATION YOU WIN!!!");
-            }else{
-                Messages.Add("Unfortunately that is the wrong password. You blow up.");
-            }
-            return false;
-        }
+    public void Help()
+    {
+      Console.Clear();
+      Console.WriteLine("[look] describe surroundings");
+      Console.WriteLine("[take <item>] take item");
+      Console.WriteLine("[use <item>] Use an item");
+      Console.WriteLine("[inventory] show what is in your inventory");
+      Console.WriteLine("[go <direction>] go towards that direction");
+      Console.WriteLine("[help] this...");
 
     }
+
+    public void Inventory()
+    {
+      Messages.Add("Current Inventory: ");
+      foreach (var item in _game.CurrentPlayer.Inventory)
+      {
+        Messages.Add($"{item.Name} - {item.Description}");
+      }
+    }
+
+    public void Look()
+    {
+      Messages.Add(_game.CurrentRoom.Name);
+      Messages.Add(_game.CurrentRoom.Description);
+      if (_game.CurrentRoom.Items.Count > 0)
+      {
+        Messages.Add("There Are a few things in this room:");
+        foreach (var item in _game.CurrentRoom.Items)
+        {
+          Messages.Add("     " + item.Name);
+        }
+      }
+      // string exits = string.Join(", ", _game.CurrentRoom.Exits.Keys);
+      // Messages.Add("There are exits to the " + exits);
+
+      // string lockedExits = "";
+      // foreach (var lockedRoom in _game.CurrentRoom.LockedExits.Values)
+      // {
+      //     lockedExits += lockedRoom.Key;
+      // }
+      // Messages.Add("There are locked exits to the " + lockedExits);
+
+    }
+
+    public void Reset()
+    {
+      string name = _game.CurrentPlayer.Name;
+      _game = new Game();
+      _game.CurrentPlayer = new Player(name);
+    }
+
+    public void Take(string itemName)
+    {
+      IItem found = _game.CurrentRoom.Items.Find(i => i.Name.ToLower() == itemName);
+      if (found != null)
+      {
+        _game.CurrentPlayer.Inventory.Add(found);
+        _game.CurrentRoom.Items.Remove(found);
+        Messages.Add($"You have taken the {itemName}");
+        return;
+      }
+      Messages.Add("Cannot find item by that name");
+    }
+
+    public void Use(string itemName)
+    {
+      var found = _game.CurrentPlayer.Inventory.Find(i => i.Name.ToLower() == itemName);
+      var foundInRoom = _game.CurrentRoom.Items.Find(i => i.Name.ToLower() == itemName);
+
+      if (found != null)
+      {
+        Messages.Add(_game.CurrentRoom.Use(found));
+      }
+      else if (itemName == "chain")
+      {
+        _game.CurrentRoom = _game.CurrentRoom.Exits["switch"];
+        Look();
+      }
+      else
+      {
+        Messages.Add("You don't have that Item");
+      }
+      // check if item is in Inventory
+      return;
+    }
+
+    public bool CrackSafe(string code)
+    {
+      if (code == "1112")
+      {
+        Messages.Add("CONGRATULATION YOU WIN!!!");
+      }
+      else
+      {
+        Messages.Add("Unfortunately that is the wrong password. You blow up.");
+      }
+      return false;
+    }
+
+  }
 }
